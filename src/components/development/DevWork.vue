50cf5a3c3f42dfd742d7b182a8a31ec95d55ad75
RobertJHog <rj.hogerbrugge@hotmail.com>
2017-11-28 10:44:42 +0100

    styling, margins

--- src/components/development/DevWork.vue
+++ src/components/development/DevWork.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="columns">
-    <div class="column">
-      <p class="title">Homerun 2017 - 2017</p>
-      <p class="subtitle"> Junior developer </p>
-      <p class="job-text"> During 4 months I have worked as a junior developer on the platform of Homerun. Under supervision they learned me the ropes on
-        HTML, CSS, Javascript and mysql within a large web application.
-        <br><br>
-        The main focus was transforming the main framework (Angular 1.X) into Vue.js. It was refactoring a lot of excisting code into the new framework.
-      </p>
-    </div>
-    <div class="column">
-      <p class="title">Your company?</p>
-      <p class="subtitle"> Junior developer </p>
-      <p class="job-text"> Always open to new opportunities in webdevelopment with HTML5, CSS3 (Sass, Less), Javascript, Ruby and libraries/frameworks such as
-        React, Vue, Angular, Rails. Very willing to learn new languages and frameworks.
-        </p>
+  <div class="devwork-container">
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <article class="tile is-child is-2 box border-solid">
+          <p class="title">Homerun 2017 - 2017</p>
+          <p class="subtitle"> Junior developer </p>
+          <p class="job-text"> During 4 months I have worked as a junior developer on the platform of Homerun. Under supervision they learned me the ropes on
+            HTML, CSS, Javascript and mysql within a large web application.
+            <br><br>
+            The main focus was transforming the main framework (Angular 1.X) into Vue.js. It was refactoring a lot of excisting code into the new framework.
+          </p>
+        </article>
+      </div>
+      <div class="tile is-parent">
+        <article class="tile is-child box border-solid">
+          <p class="title">Your company?</p>
+          <p class="subtitle"> Junior developer </p>
+          <p class="job-text"> Always open to new opportunities in webdevelopment with HTML5, CSS3 (Sass, Less), Javascript, Ruby and libraries/frameworks such as
+            React, Vue, Angular, Rails. Very willing to learn new languages and frameworks.
+            </p>
+        </article>
+      </div>
     </div>
   </div>
 </template>
@@ -28,22 +34,30 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+.devwork-container {
+  padding: 2em auto;
+  margin: 0em 7em 4em 7em;
+}
+
+.border-solid {
+  border: #d0772b solid 4px;
+  -webkit-box-shadow: 0px 7px 4px #777;
+  -moz-box-shadow: 0px 7px 4px #777;
+  box-shadow: 0px 7px 4px #777;
+}
+
 .title {
   margin-bottom: 1em;
 }
 
-.columns {
-  margin: 2em auto;
-}
-
-.column {
+.tile {
   margin: 2em;
   width: 20em;
-  height: 60vh;
   opacity: .5;
+  font-size: 20px
 }
 
-.column:hover {
+.tile:hover {
   opacity: 1;
 }
 
--- src/components/sales/SalesWork.vue
+++ src/components/sales/SalesWork.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="saleswork-container">
-    <div class="columns">
-      <div class="column">
-        <p class="title">Iron Mountain</p>
-        <p class="title">2010 - 2016</p>
-        <p class="subtitle"> Business Development Executive / Manager </p>
-        <p class="job-text"> As Business Development Manager I was responsible for managing the corporate accounts of
-          Iron Mountain Benelux and generating new revenue from existing and new accounts. This includes the commercial
-           management of tenders, re-tenders, and price negotiations in addition to the relationship between Iron Mountain,
-           our clients and internal stakeholders.
-          <br><br>
-          My main focus was on corporate organisations in banking & finance. I also have extensive experience in verticals such as
-          pharmacy, government and education.
-        </p>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <article class="tile is-6 is-child box border-solid">
+          <p class="title">Iron Mountain</p>
+          <p class="title">2010 - 2016</p>
+          <p class="subtitle"> Business Development Manager </p>
+          <p class="job-text"> As Business Development Manager I was responsible for managing the corporate accounts of
+            Iron Mountain Benelux and generating new revenue from existing and new accounts. This includes the commercial
+             management of tenders, re-tenders, and price negotiations in addition to the relationship between Iron Mountain,
+             our clients and internal stakeholders.
+            <br><br>
+            My main focus was on corporate organisations in banking & finance. I also have extensive experience in verticals such as
+            pharmacy, government and education.
+          </p>
+          <p class="subtitle"> Business Development Executive </p>
+          <p class="job-text"> In this role I was responsible for generating new business and managing midmarket & corporate accounts
+             in the Benelux. My main focus is on verticals such as government, pharmacy, financials & education providing solid solutions
+             on Records Management, Digitalisation and Tape Management.
+          </p>
+        </article>
       </div>
-      <div class="column">
-        <p class="title">De Breed & Partners</p>
-        <p class="title">2007 - 2010</p>
-        <p class="subtitle"> Consultant & Project Manager </p>
-        <p class="job-text">  During my time at the De Breed & Partners I helped technology innovative companies (B2B) reaching their market and increase their revenue.
+      <div class="tile is-parent">
+        <article class="tile is-6 is-child box border-solid">
+          <p class="title">De Breed & Partners</p>
+          <p class="title">2007 - 2010</p>
+          <p class="subtitle"> Consultant & Project Manager </p>
+          <p class="job-text">  During my time at the De Breed & Partners I helped technology innovative companies (B2B) reaching their market and increase their revenue.
           In collaboration with our customers I thought out telemarketing strategies, advised customers on their proposition and trained telemarketers & junior consultants.
           I lead successful projects for:
           </p>
-          <p> Document- & workflow management solutions </p>
-          <p> WBSO, ESF, Innovatiebox grants </p>
-          <p> Contract & Service Level Agreement managing tools </p>
+          <p class="subtitle"> Document- & workflow management solutions </p>
+          <p class="subtitle"> WBSO, ESF, Innovatiebox grants </p>
+          <p class="subtitle"> Contract & Service Level Agreement managing tools </p>
+        </article>
       </div>
     </div>
   </div>
@@ -41,24 +50,31 @@ export default {
 
 .saleswork-container {
   padding: 4em auto;
+  margin: 0em 3em 4em 3em;
 }
 
 .title {
   margin-bottom: 1em;
 }
 
-.columns {
-  margin: 0 auto;
+.job-text {
+  padding-bottom: 2em;
 }
 
-.column {
+.border-solid {
+  border: #697de3 solid 4px;
+  -webkit-box-shadow: 0px 7px 4px #777;
+  -moz-box-shadow: 0px 7px 4px #777;
+  box-shadow: 0px 7px 4px #777;
+}
+
+.tile {
   margin: 2em;
-  width: 20em;
-  height: 60vh;
+  width: 30em;
   opacity: .5;
 }
 
-.column:hover {
+.tile:hover {
   opacity: 1;
 }
 
